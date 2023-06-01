907c2d66da01acef161b1435d583808229bc5702
Gifty <giftyaningg@gmail.com>
2023-06-01 03:30:36 +0100

    Updated Html, Css  and JavaScript

--- src/index.js
+++ src/index.js
@@ -1,144 +1,27 @@
-//function to give the current time and date.
+function displayTemperature(response) {
 
-let now = new Date();
+console.log(response.data);
 
-    let time = document.querySelector("#set-time");
+let temperatureElement = document.querySelector("#temperature");
 
-    let current = document.querySelector("h3");
+let cityElement = document.querySelector("#city-name");
 
-    let date = now.getDate();
+let humidityElement = document.querySelector("#humid");
 
-    let year = now.getFullYear();
+let windElement = document.querySelector("#speed");
 
-    let hour = now.getHours();
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
-    if (hour < 10) {
+cityElement.innerHTML = response.data.name;
 
-    hour = `0${hour}`;
+humidityElement.innerHTML = response.data.main.humidity;
 
-    }
-
-    let minutes = now.getMinutes();
-
-    if (minutes < 10) {
-
-    minutes = `0${minutes}`;
-
-    }
-
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    let day = days[now.getDay()];
-
-let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-    let month = months[now.getMonth()];
-
-    current.innerHTML = `${day}`;
-
-//Function to show current temperature.
-
-const weatherIcons = {
-  "Clear": "☀️",
-  "Clouds": "☁️",
-  "Rain": "🌧️",
-  "Drizzle": "🌦️",
-  "Thunderstorm": "⛈️",
-  "Snow": "❄️",
-  "Mist": "🌫️",
-};
-
-function showTemperature(response) {
-
-    let temperature = Math.round(response.data.main.temp);
-
-    let temperatureElement = document.querySelector("#temperature");
-
-    temperatureElement.innerHTML = `${temperature}ºC`;
-
-
-
-    let weatherCondition = response.data.weather[0].main;
-    let weatherIconElement = document.querySelector(".weather-icon");
-    weatherIconElement.innerHTML = weatherIcons[weatherCondition] || "❓";
-}
-
-//Function to get the weather data for a specific city.
-
-function getWeather(cityName) {
-
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3980a7c8f2a782241a093131b099f993&units=metric`;
-
-    axios.get(apiUrl).then(showTemperature);
+windElement.innerHTML = Math.round(response.data.wind.speed);
 
 }
 
-//Function for search event.
+let apiKey = "f42a88be335e928af3463b3b12ed0e27"
 
-function search(event) {
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=f42a88be335e928af3463b3b12ed0e27&units=metric`"
 
-    event.preventDefault();
-
-    let searchInput = document.querySelector("#search-text-input");
-
-    let h1 = document.querySelector("#city-name");
-
-    h1.innerHTML = `${searchInput.value}`;
-
-    let cityName = searchInput.value;
-
-    getWeather(cityName);
-
-    getCurrentTime(cityName);
-
-    searchInput.value = "";
-
-}
-
-let form = document.querySelector("#search-form")
-
-    form.addEventListener("submit", search);
-
-    // Function to handle the page load event
-
-    function onPageLoad() {
-
-    updateDateTime();
-
-    setInterval(updateDateTime, 1000); // Update date and time every second
-
-}
-
-window.addEventListener("load", onPageLoad);
-
-    function getCurrentTime(cityName) {
-
-    let apiUrl = `http://worldtimeapi.org/api/timezone/Europe/${cityName}`;
-
-    axios.get(apiUrl).then(showCurrentTime).catch(handleError);
-
-}
-
-//To get current time
-
-function showCurrentTime(response) {
-
-    let currentTimeElement = document.querySelector("#set-clock");
-
-    let currentDateTime = new Date(response.data.datetime);
-
-    let hour = currentDateTime.getHours().toString().padStart(2, "0");
-
-    let minutes = currentDateTime.getMinutes().toString().padStart(2, "0");
-
-    currentTimeElement.textContent = `${hour}:${minutes}`;
-
-}
-
-//To handle errors if necessary
-
-function handleError(error) {
-
-    console.log(error);
-
-}
+axios.get(apiUrl).then(displayTemperature);
